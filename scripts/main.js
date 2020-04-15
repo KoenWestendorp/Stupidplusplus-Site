ef4ab92eb0b3ce396cf92a10678374a8e72a3f4e
Koen Westendorp <koensswestendorp@icloud.com>
2020-04-15 22:01:09 +0200

    Padding and other changes

--- scripts/main.js
+++ scripts/main.js
@@ -15,24 +15,24 @@ function showImage(index) {
     slides[index].classList.toggle("show");
 }
   
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
+    }
 
-      var dropdowns = document.getElementsByClassName("dropbtn");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('rotate')) {
-          openDropdown.classList.remove('rotate');
-        }
+    var dropdowns = document.getElementsByClassName("dropbtn");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('rotate')) {
+        openDropdown.classList.remove('rotate');
       }
     }
   }
+}
